deeed238eec6272a77c64b7aa65cad45ad848c97
Matteo Juen <matteo.juen@ematric.com>
2025-12-16 09:45:21 +0100

    feat: settings for mail and registration

--- lib/models/settings/settings_model.dart
+++ lib/models/settings/settings_model.dart
@@ -1,15 +1,27 @@
 class SettingsModel {
   String url;
+  bool registrationEnabled;
+  String? supportEmail;
 
-  SettingsModel({required this.url});
+  SettingsModel({
+    required this.url,
+    required this.registrationEnabled,
+    this.supportEmail,
+  });
 
   factory SettingsModel.fromJson(Map<String, dynamic> json) {
     return SettingsModel(
       url: json['url']?.toString() ?? 'https://api.example.com/',
+      registrationEnabled: json['registrationEnabled'] ?? false,
+      supportEmail: json['supportEmail']?.toString(),
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {'url': url};
+    return {
+      'url': url,
+      'registrationEnabled': registrationEnabled,
+      'supportEmail': supportEmail,
+    };
   }
 }
